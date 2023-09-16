ecaad6ae69a58bcc688dc7921a614e1bc64ff58b
Vasyl Mokhnatchuk <144223353+Vasyl8838@users.noreply.github.com>
2023-09-16 05:33:09 +0300

    Add files via upload

---
+++ project2.js
@@ -0,0 +1,16 @@
+let test1 = 1234;
+  test2 = 'Привіт!';
+  test3 = 'underfinded';
+  test4 = false;
+  test5 = NaN;
+  
+console.log(test1, test2, test3, test4, test5);
+
+let info = {
+    Name: 'Vasyl',
+    Surname: 'Mokhnatchuk',
+    Age: '17',
+    Home: 'Ukraine, Ivano-Frankivsk',
+};
+
+console.table([info]);
